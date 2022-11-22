9a26ac7ff45bf1cc84c70c5775bae0f68b01e91d
God <abhinabpratap1311@gmail.com>
2022-11-22 17:31:46 +0530

    Temp

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
-self.addEventListener('push', () => {
-  self.registration.showNotification('Hello world!', options);
+self.addEventListener('push', function(e){
+  var  options ={
+      body:'this is test meaasage',
+      icon:'image/example.png',
+      vibrate:[100,50,100],
+      data:{
+          dateOfArrival: Date.now(),
+          primaryKey: '2'
+      },
+      actions: [
+          {action: 'explore', title:'explore this world',
+            icon:'image/example.png'},
+          {action: 'close', title: 'close',
+            icon:'image/example.png'},
+      ]
+  };
+  e.waitUntil(
+  self.ServiceWorkerRegistration.showNotification('hello world',options)
+  );
 });
